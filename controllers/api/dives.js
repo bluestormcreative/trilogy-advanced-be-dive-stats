65615c242fbb993074c6e4461d9ed31c35595993
Max MacMillan <max@bluestormcreative.com>
2021-06-07 23:27:39 -0700

    Wire up all the dives endpoints

--- controllers/api/dives.js
+++ controllers/api/dives.js
@@ -4,10 +4,22 @@ const { Dive } = require('../../models');
 /* use Express.js' router object to create a GET
  endpoint for the getLatest query.
  Using async/await here means we don't need to 
- write the returned Promise as a nested callback */
+ write the returned Promise as a nested callback. 
+ Note the route is '/' */
 router.get('/', async (req, res) => {
   const { rows } = await Dive.getLatest();
   res.json(rows);
+});
+
+/* the stats route accepts a query param for most_active_month */
+router.get('/stats', async (req, res) => {
+  if (req.query.data === 'most_active_month') {
+    const { rows } = await Dive.getActiveMonth();
+    res.json(rows[0]);
+  }
+  else {
+    res.status(404).end();
+  }
 });
 
 module.exports = router;
