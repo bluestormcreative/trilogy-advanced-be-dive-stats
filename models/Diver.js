c402a485335a54842f955df8d79ba33d5834322c
Max MacMillan <max@bluestormcreative.com>
2021-06-07 23:35:36 -0700

    Add divers models and api routes

--- models/Diver.js
+++ models/Diver.js
@@ -5,9 +5,21 @@ class Diver {
     return db.query(`SELECT * FROM divers ORDER BY last_name`);
   }
 
+  // This uses a prepared statement (with the pg module)
+  // to sanitize the ID value.
   getOne({ id }) {
     return db.query(
       `SELECT * FROM divers WHERE id = $1`,
+      [ id ]
+    );
+  }
+
+  getTotalDives({ id }) {
+    return db.query(
+      `SELECT COUNT(diver_id) AS number_of_dives
+      FROM dives
+      WHERE diver_id = $1
+      GROUP BY diver_id`,
       [ id ]
     );
   }
